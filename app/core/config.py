ae352e90a9971d77f6a3973e20e0989b29442d3b
manfre <deividmanfre@gmail.com>
2025-10-24 11:10:04 -0300

    creat db script and modifying the config script

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,10 @@
-from pydantic import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
     currency_api_key: str
     currency_api_url: str
-    
-    class Config:
-        env_file = ".env"
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
