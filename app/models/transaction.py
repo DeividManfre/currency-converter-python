c4bff981b0a54a5fad20e82c156d9f85b4672044
manfre <deividmanfre@gmail.com>
2025-10-24 20:44:55 -0300

    feat: define Transaction model for db persistence

---
+++ app/models/transaction.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from datetime import datetime
+from app.db.database import base
+
+
+class Transaction(base):
+    __tablename__ = "transactions"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    from_currency = Column(String, nullable=False)
+    to_currency = Column(String, nullable=False)
+    from_value = Column(Float, nullable=False)
+    to_value = Column(Float, nullable=False)
+    rate = Column(Float, nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
