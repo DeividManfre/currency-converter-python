641f02381ce0014b485d737af23303869ef0db9e
manfre <deividmanfre@gmail.com>
2025-10-25 01:05:02 -0300

    feat: create Pydantic schemas for request and response validation

---
+++ app/schemas/transaction.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class TransactionCreate(BaseModel):
+    user_id: int
+    from_currency: str
+    to_currency: str
+    value: float
+
+class TransactionResponse(BaseModel):
+    id: int
+    user_id: int
+    from_currency: str
+    to_currency: str
+    from_value: float
+    to_value: float
+    rate: float
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
